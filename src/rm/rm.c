a4a056b47172fb758cc71d301f435acf309343b3
greycloudss <davidclouddd@gmail.com>
2025-12-27 16:02:41 +0200

    fixed some bs

--- src/rm/rm.c
+++ src/rm/rm.c
@@ -1,10 +1,10 @@
 #include "rm.h"
 #include "../utils/utils.h"
 
-Memory physicalMemory;
-CPU realCPU;
-HardDisk hardDisk;
-Timer systemTimer;
+Memory* physicalMemory;
+CPU* realCPU;
+HardDisk* hardDisk;
+Timer* systemTimer;
 
 bool initKernel(Memory* mem, CPU* cpu, HardDisk* hard) {
     if (!mem || !cpu || !hard) return false;
@@ -14,7 +14,7 @@ bool initKernel(Memory* mem, CPU* cpu, HardDisk* hard) {
     clearAllInterrupts();
 
     initDisk(hard, 0);
-    initTimer(&systemTimer, TICK_LIMIT);
+    initTimer(systemTimer, TICK_LIMIT);
     IOinit();
 
     printf("[RM] Initialization complete.\n");
@@ -23,7 +23,7 @@ bool initKernel(Memory* mem, CPU* cpu, HardDisk* hard) {
 void initRealMachine(void) {
     _log("[RM] Initializing Real Machine...\n");
 
-    if (initKernel(&physicalMemory, &realCPU, &hardDisk))
+    if (initKernel(physicalMemory, realCPU, hardDisk))
         _log("[RM] Initialization complete.\n");
     else
         _log("[RM] Initialization failed.\n");
@@ -32,7 +32,7 @@ void initRealMachine(void) {
 void startVirtualMachine(void) {
     printf("[RM] Starting Virtual Machine...\n");
 
-    VirtualMachine* vm = createVM(&hardDisk, &physicalMemory);
+    VirtualMachine* vm = createVM(hardDisk, physicalMemory);
     if (!vm) {
         printf("[RM] VM initialization failed.\n");
         return;
